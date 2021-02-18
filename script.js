a4194c953ddc16e7f0a04a2777e3004d2f4e942e
raymondlamb <Raymondlamb91@gmail.com>
2021-02-18 19:52:15 +0000

    Temp addition ^X

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var pricipal = document.getElementById("principal");
+    var rate = document.getElementById("rate");
+    var years = document.getElementById("years");
+    var convYears = number(years);
+    var interest = principal * convYears * rate/100;
 }
-        
+
+//Slider
+    function updateSlider(slideAmount) {
+        var sliderDiv = document.getElementById("principal");
+        sliderDiv.innerHTML = slideAmount;
+    }

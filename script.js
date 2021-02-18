0e7f62acd9896a5d00f9b55f0297460240cecbdc
raymondlamb <Raymondlamb91@gmail.com>
2021-02-18 20:00:27 +0000

    Another save point

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ function compute()
     var interest = principal * convYears * rate/100;
 }
 
-//Slider
-    function updateSlider(slideAmount) {
-        var sliderDiv = document.getElementById("principal");
-        sliderDiv.innerHTML = slideAmount;
-    }
+function updateSlider(slideAmount) {
+    var sliderDiv = document.getElementById("sliderAmount");
+    sliderDiv.innerHTML = slideAmount;
+}

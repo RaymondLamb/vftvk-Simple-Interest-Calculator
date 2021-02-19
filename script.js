d53758cceabcb23598852a6bca70e60d2d021f26
raymondlamb <raymondlamb91@gmail.com>
2021-02-19 11:59:33 +0000

    Updated the script for calculating the interest

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    var pricipal = document.getElementById("principal");
-    var rate = document.getElementById("rate");
-    var years = document.getElementById("years");
-    var convYears = number(years);
-    var interest = principal * convYears * rate/100;
-}
-
-function updateSlider(slideAmount) {
-    var sliderDiv = document.getElementById("sliderAmount");
-    sliderDiv.innerHTML = slideAmount;
+function compute(){
+     p = doccument.getElementByID("principal").value 
 }
